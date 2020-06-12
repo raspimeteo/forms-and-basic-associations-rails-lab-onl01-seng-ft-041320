1d4e01fb9391f4a548865bf947d2659c1ed237af
raspimeteo <jjczon@gmail.com>
2020-06-12 13:45:36 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
   def index
     @songs = Song.all
@@ -12,7 +13,11 @@ class SongsController < ApplicationController
   end
 
   def create
+    # byebug
+    # artist = Artist.find_or_create_by(name: song_params[:artist_iname])
+    # genre = Genre.find(params[:song][:genre_id])
     @song = Song.new(song_params)
+    
 
     if @song.save
       redirect_to @song
@@ -47,7 +52,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, notes: [])
   end
+
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+ 
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  	 
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes=(notes)  
+    notes.each do |note|
+      if !note.empty?
+        note = Note.new(:content => note, :song_id => self.id)
+        note.save 
+        self.notes << note 
+      end
+    end
+  end
+
 end
